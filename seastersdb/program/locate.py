6c7f639f58fef03991104674377e864a9880816e
Quentin DESMET <desmetque@gmail.com>
2026-03-05 12:56:06 +0100

    feat: seastersdb-locate

---
+++ seastersdb/program/locate.py
@@ -0,0 +1,31 @@
+import argparse
+import importlib.util
+import sys
+from pathlib import Path
+
+
+def main() -> None:
+    parser = argparse.ArgumentParser(
+        description=(
+            "Tell seastersdb where the database is located."
+        )
+    )
+    parser.add_argument("path", help="path to the database")
+    args = parser.parse_args()
+
+    # Package existence check
+    spec = importlib.util.find_spec("seastersdb")
+    if not spec:
+        sys.stderr.write("Failure: Package `seastersdb` not found.\n")
+        sys.stderr.write("Install it then re-run.\n")
+        sys.exit(1)
+
+    # Write data directory in 'seastersdb/data/path.txt'
+    file = Path(spec.origin).parent / "data" / "path.txt"
+    file.write_text(str(Path(args.path).resolve()))
+    sys.stdout.write("API configuration completed!\n")
+    sys.exit(0)
+
+
+if __name__ == "__main__":
+    main()
